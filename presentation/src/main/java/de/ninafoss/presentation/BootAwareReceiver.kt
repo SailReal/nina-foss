c3b4f1094cab890ff1603d40246538087cd36c62
Julian Raufelder <Julian@Raufelder.com>
2021-11-28 00:50:45 +0100

    Start service again after reboot

--- presentation/src/main/java/de/ninafoss/presentation/BootAwareReceiver.kt
+++ presentation/src/main/java/de/ninafoss/presentation/BootAwareReceiver.kt
@@ -3,13 +3,18 @@ package de.ninafoss.presentation
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import de.ninafoss.presentation.ui.activity.SplashActivity
 
 class BootAwareReceiver : BroadcastReceiver() {
 
 	override fun onReceive(context: Context, intent: Intent) {
 		when {
 			intent.action.equals(Intent.ACTION_BOOT_COMPLETED, ignoreCase = true) -> {
-				TODO("Not yet implemented") // --> start service
+				context.startActivity(
+					Intent(context, SplashActivity::class.java)
+						.addFlags(FLAG_ACTIVITY_NEW_TASK)
+				)
 			}
 		}
 	}

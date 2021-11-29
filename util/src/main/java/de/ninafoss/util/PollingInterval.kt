64b766c06f99c0e44d497194f81d1d6268b02f40
Julian Raufelder <Julian@Raufelder.com>
2021-11-29 21:19:21 +0100

    Add 5 minute polling interval

--- util/src/main/java/de/ninafoss/util/PollingInterval.kt
+++ util/src/main/java/de/ninafoss/util/PollingInterval.kt
@@ -3,6 +3,7 @@ package de.ninafoss.util
 import java.util.concurrent.TimeUnit
 
 enum class PollingInterval(private val duration: Int, private var unit: TimeUnit) {
+	FIVE_MINUTES(5, TimeUnit.MINUTES),
 	FIFTEEN_MINUTES(15, TimeUnit.MINUTES),
 	ONE_HOUR(1, TimeUnit.HOURS),
 	SIX_HOUR(6, TimeUnit.HOURS);
